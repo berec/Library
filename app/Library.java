ef5107029a0a2eed89f6e85f189657b3de032a13
berec <norbert.bruc@gmail.com>
2017-12-09 22:46:13 +0100

    added tables

--- app/Library.java
+++ app/Library.java
@@ -8,18 +8,21 @@ public class Library
     {
         final String appName ="Biblioteka v0.5";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk SIenkiewicz",
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk SIenkiewicz",
                 2010, 296, "Greg", "97898774647848");
-        Book book2 = new Book("Java", "Jushua Beck", 2009, 352,
+        books[1] = new Book("Java", "Jushua Beck", 2009, 352,
                 "Helion", "998849847398745");
-        Book book3 = new Book("SCJP Java", "Bert Bates", 2008, 851,
+        books[2] = new Book("SCJP Java", "Bert Bates", 2008, 851,
                 "Osborne Media", "98374837646374");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("System może przechowywać do: " + books.length + " książek");
 
     }
 }
