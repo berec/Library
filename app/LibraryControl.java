9da25cfb63b3563f558a81cecbf841577383dde1
berec <norbert.bruc@gmail.com>
2018-02-12 22:07:27 +0100

    Added magazines in LibraryControl

--- app/LibraryControl.java
+++ app/LibraryControl.java
@@ -3,18 +3,25 @@ package app;
 import utils.DataReader;
 import data.Book;
 import data.Library;
+import data.Magazine;
 
 public class LibraryControl
 {
-    // stałe
     public static final int EXIT = 0;
     public static final int ADD_BOOK = 1;
-    public static final int PRINT_BOOKS = 2;
+    public static final int ADD_MAGAZINE = 2;
+    public static final int PRINT_BOOKS = 3;
+    public static final int PRINT_MAGAZINES = 4;
 
+    private DataReader dataReader;
 
-    /*
-     * Główna pętla programu, która pozwala na wybór opcji i interakcję
-     */
+    private Library library;
+
+    public LibraryControl()
+    {
+        dataReader = new DataReader();
+        library = new Library();
+    }
 
     public void controlLoop()
     {
@@ -27,8 +34,14 @@ public class LibraryControl
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZINE:
+                    addMagazine();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZINES:
+                    printMagazines();
                     break;
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadź ponownie");
@@ -43,20 +56,11 @@ public class LibraryControl
         System.out.println("Wybierz opcję: ");
         System.out.println(EXIT + " - wyjście z programu");
         System.out.println(ADD_BOOK + " - dodanie nowej książki");
+        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
         System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
+        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
     }
 
-    // zmienna do komunikacji z użytkownikiem
-    private DataReader dataReader;
-
-    // "biblioteka" przechowująca dane
-    private Library library;
-
-    public LibraryControl()
-    {
-        dataReader = new DataReader();
-        library = new Library();
-    }
 
     private void addBook()
     {
@@ -67,5 +71,16 @@ public class LibraryControl
     private void printBooks()
     {
         library.printBooks();
+    }
+
+    private void addMagazine()
+    {
+        Magazine magazine = dataReader.readAndCreatemagazine();
+        library.addMagazine(magazine);
+    }
+
+    private void printMagazines()
+    {
+        library.printMagazines();
     }
 }
