96984c86bf817c4a379f396a527c16f1e500866b
berec <norbert.bruc@gmail.com>
2018-02-12 21:56:17 +0100

    Added magazines in Library class

--- data/Library.java
+++ data/Library.java
@@ -3,11 +3,35 @@ package data;
 public class Library {
 
     public static final int MAX_BOOKS = 1000;
+    public static final int MAX_MAGAZINES = 1000;
     private Book[] books;
+    private Magazine[] magazines;
     private int booksNumber;
+    private int magazinesNumber;
+
+    public int getBooksNumber()
+    {
+        return booksNumber;
+    }
+
+    public Book[] getBooks()
+    {
+        return books;
+    }
+
+    public int getMagazinesNumber()
+    {
+        return magazinesNumber;
+    }
+
+    public Magazine[] getMagazines()
+    {
+        return magazines
+    }
 
     public Library() {
         books = new Book[MAX_BOOKS];
+        magazines = new Magazine[MAX_MAGAZINES]
     }
 
     public void addBook(Book book)
@@ -22,13 +46,18 @@ public class Library {
         }
     }
 
-    public int getBooksNumber() {
-        return booksNumber;
+    public void addMagazine(Magazine magazine)
+    {
+        if(magazinesNumber < MAX_MAGAZINES)
+        {
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++
+        } else
+        {
+            System.out.println("Maksymalna liczba magazynów została osiągnięta");
+        }
     }
 
-    public Book[] getBooks() {
-        return books;
-    }
 
     public void printBooks() {
         if(booksNumber == 0) {
@@ -36,6 +65,18 @@ public class Library {
         }
         for(int i=0; i<booksNumber; i++) {
             books[i].printInfo();
+        }
+    }
+
+    public void printMagazines()
+    {
+        if(magazinesNumber == 0)
+        {
+            System.out.println("Brak magazynów w bibliotece");
+        }
+        for(int i=0; i<magazinesNumber; i++)
+        {
+            magazines[i].printInfo();
         }
     }
 }
